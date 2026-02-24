3e2b635d28dd941f2305c960c1b960114d36e69b
db-nawaru-plataforma-oficial <dbnawaruweb@gmail.com>
2026-02-23 18:55:52 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,14 +1,11 @@
-// Configuración Global
+// Configuración Maestra
 const SUPABASE_URL = 'https://shnwanhkuphlcevxnlvh.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_GqwY_VwSleM0MwNoshDQsQ_Qdl0lOu2';
 
+// Credenciales ÚNICAS
 const ADMIN_USERNAME = 'Nawaru';
 const ADMIN_PASSWORD = 'JAO2026';
 
-// Inicialización
+// Inicialización de Supabase
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 window.supabase = supabase;
-
-// Variables de respaldo
-window.USER_BACKUP = "Nawaru";
-window.PASS_BACKUP = "JAO2026";
